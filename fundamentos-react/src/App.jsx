64aa326e8d0c82d60c6bfcdc9a949af64fb10e95
unknown <lucas.aferreira@bandtec.com.br>
2022-10-06 02:10:51 -0300

    feat - criando component card e estilizando

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,23 +1,36 @@
 import React from "react";
 
-import Primeiro from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
-import Fragmento from './components/basicos/Fragmento'
-import Aleatorio from './components/basicos/Aleatorio'
+import Card from "./components/layout/Card";
+import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
+import Fragmento from './components/basicos/Fragmento';
+import Aleatorio from './components/basicos/Aleatorio';
 
 
 export default (_) => (
     <div id='app'>
         <h1>Fundamentos react</h1>
-        <Aleatorio 
-        min = {1}
-        max = {10}
-        />
-        <Fragmento />
-        <ComParametro
-            titulo="Situacao do aluno: "
-            aluno="Lucas"
-            nota={9.9} />
-        <Primeiro></Primeiro>
+
+        <h1>Fundamentos React</h1>
+
+        <Card titulo="#04 - Desafio Aleatorio">
+            <Aleatorio min={1} max={10} />
+        </Card>
+
+        <Card titulo="#03 - Fragmento">
+            <Fragmento />
+        </Card>
+
+        <Card titulo="#02 - Com parametro">
+            <ComParametro
+                titulo="Situacao do aluno: "
+                aluno="Lucas"
+                nota={9.9} />
+
+        </Card>
+
+        <Card titulo="#01 - Primeiro">
+            <Primeiro></Primeiro>
+        </Card>
     </div >
 );
